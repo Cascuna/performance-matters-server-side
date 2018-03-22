3933635f596138f78e0ba98fa605c54995f96b25
Cascuna <amvvanderwal@live.nl>
2018-03-22 23:23:33 +0100

    Finished progressive enhancement javascript for list of streets

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,19 @@
-const select = require("../../building/sparql")
+var select = require("../../building/sparql")
 
 {
     var yearSelect = document.getElementById("yearselection")
-
+    
+    // TODO : Change with virtual dom :)
     yearSelect.addEventListener("change", (event) => {
-        select.allBuildingsByYear(1970).then((buildings) => {
+
+        select.allBuildingsByYear(event.target.value).then((buildings) => {
+            var buildingList = document.getElementById('buildings-container')
+            buildingList.innerHTML = ''
             for(var building of buildings) {
                 var buildingEl = document.createElement("div")
-                console.log(buildingEl)
-                document.body.appendChild(buildingEl)
+                buildingEl.innerHTML = building.label.value
+                buildingList.appendChild(buildingEl)
+                console.log(buildingList)
             }
         })
     })
